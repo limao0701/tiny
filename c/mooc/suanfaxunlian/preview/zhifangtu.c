d4b634c41daa8a53d291222a46549b675e4e6496
nemo <nemo@nemodeMacBook-Pro.local>
2020-04-30 13:27:38 +0800

    add test

--- c/mooc/suanfaxunlian/preview/zhifangtu.c
+++ c/mooc/suanfaxunlian/preview/zhifangtu.c
@@ -1,41 +1,52 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 unsigned int find_small_index(unsigned int a[],unsigned int n);
 unsigned int force(unsigned int a[],unsigned int n);
-unsigned int recursion(unsigned int a[],unsigned int n);
+unsigned int recursion(unsigned int a[],int n);
 
 int main(){
-	unsigned int n;
+        srand(time(NULL));
+        unsigned int max_len=1;
+	scanf("%u",&max_len);
+        unsigned int n=max_len;
+        unsigned int a[n];
+        for(unsigned int i=0;i<n;i++){
+		srand(time(NULL));
+		a[i]=(unsigned int)rand()%32767;
+	}
+/*	unsigned int n;
 	scanf("%u",&n);
 	unsigned int a[n];
 	for(unsigned int i=0;i<n;i++){
     		scanf("%u",&a[i]);
 	}
-printf("inputed");
+*/
 	printf("%u",force(a,n));
-        printf("%u",recursion(a,n));
+        printf("\r\n%u",recursion(a,(int)n));
 	return 0;
 }
 
 
-unsigned int recursion(unsigned int a[],unsigned int n){
+unsigned int recursion(unsigned int a[],int n){
 	if(n==1){
 		return a[0];
 	}
-	if(n==0){
+	if(n<=0){
 		return 0;
 	}
 	unsigned int small=find_small_index(&a[0],n);
-        unsigned int squal=a[small]*n;
-	unsigned int left_squal=recursion(&a[0],small-1);
-        unsigned int right_squal=recursion(&a[small+1],n-small-1);
-        squal=(squal < left_squal)? left_squal:squal;
-        squal=(squal < right_squal)? right_squal:squal;
+	unsigned int squal=a[small]*(unsigned int )n;
+	unsigned int left_squal=recursion(&a[0],(int)small-1);
+	unsigned int right_squal=recursion(&a[small+1],n-(int)small-1);
+	squal=(squal < left_squal)? left_squal:squal;
+	squal=(squal < right_squal)? right_squal:squal;
 	return squal;
 	
 }
 unsigned int force(unsigned int a[],unsigned int n){
-printf("in foce");
         unsigned int large=0;
 	unsigned int small=0;
 	unsigned int squal=0;
